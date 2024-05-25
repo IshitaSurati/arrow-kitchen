3de4edb36c7bfa1f1896e66145cf8ebde2efc268
Ishitasurat25 <ishitasurati83@gmail.com>
2024-05-25 14:26:19 +0530

    update

--- script.js
+++ script.js
@@ -8,21 +8,7 @@ $(document).ready(function () {
     });
   }
 });
-// form
-function sendEmail() {
-  Email.send({
-    SecureToken:"7a34bf9f-4032-4ada-9375-9c4d18e5c357",
-    To: 'arrownine9@gmail.com',
-    From: document.getElementById("email").value,
-    Subject: "New Contact Form for Enquiry",
-    Body: "Name : " + document.getElementById("fullName").value +
-      "<br>Email : " + document.getElementById("email").value +
-      "<br>MobileNumber : " + document.getElementById("mobileNumber").value +
-      "<br>Email : " + document.getElementById("message").value 
-}).then(
-        message => alert("Message Send Succesfully!!!")
-      );
-}
+
 
 
 
